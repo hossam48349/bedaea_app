762e92703bece1044a545db5a70247fd988171ea
Hossam Hassan Abd Al Bari <93398238+hossam48349@users.noreply.github.com>
2026-05-07 17:14:10 +0300

    Enhance app.js with middleware and improved response
    Added middleware for CORS, security, and logging. Updated root route response format.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,49 @@
 const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const authRoutes = require("./modules/auth/auth.routes");
+const errorHandler = require("./middlewares/error.middleware");
 
 const app = express();
 
+app.use(cors());
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false
+  })
+);
+
+app.use(morgan("dev"));
+
 app.use(express.json());
 
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send("API running");
+  res.status(200).json({
+    success: true,
+    message: "Bidaea API is running"
+  });
 });
+
+
+app.use("/api/v1/auth", authRoutes);
+
+
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found"
+  });
+});
+
+app.use(errorHandler);
 
 module.exports = app;
