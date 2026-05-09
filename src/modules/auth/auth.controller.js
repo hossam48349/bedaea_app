b797690e5dc8937132980eb58de70b8ab2e60b29
Hossam Hassan <hossamhassanabdalbari@gmail.com>
2026-05-09 22:56:17 +0300

    separate customer and merchant models

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,114 +1,121 @@
 const authService = require("./auth.service");
-const Customer = require("../models/Customer");
-const Merchant = require("../models/Merchant");
+const Customer = require("../../models/Customer");
+const Merchant = require("../../models/Merchant");
 
 const {
   registerCustomerSchema,
   registerMerchantSchema,
-  loginSchema
+  loginSchema,
 } = require("./auth.validation");
 
-exports.registerCustomer = async (req, res) => {
-  const {
-    fullName,
-    centerName,
-    villageName,
-    marketName,
-    phone,
-    password,
-    confirmPassword,
-    agreedToTerms,
-  } = req.body;
+const registerCustomer = async (req, res, next) => {
+  try {
+    const {
+      fullName,
+      centerName,
+      villageName,
+      marketName,
+      phone,
+      password,
+      confirmPassword,
+      agreedToTerms,
+    } = req.body;
 
-  if (password !== confirmPassword) {
-    return res.status(400).json({
-      success: false,
-      message: "كلمة المرور غير متطابقة",
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "كلمة المرور غير متطابقة",
+      });
+    }
+
+    const customer = await Customer.create({
+      fullName,
+      centerName,
+      villageName,
+      marketName,
+      phone,
+      password,
+      agreedToTerms,
     });
-  }
 
-  const customer = await Customer.create({
-    fullName,
-    centerName,
-    villageName,
-    marketName,
-    phone,
-    password,
-    agreedToTerms,
-  });
-
-  res.status(201).json({
-    success: true,
-    message: "تم إنشاء حساب العميل بنجاح",
-    data: {
-      user: {
-        id: customer._id,
-        type: "customer",
-        fullName: customer.fullName,
-        phone: customer.phone,
-        centerName: customer.centerName,
-        villageName: customer.villageName,
-        marketName: customer.marketName,
+    res.status(201).json({
+      success: true,
+      message: "تم إنشاء حساب العميل بنجاح",
+      data: {
+        user: {
+          id: customer._id,
+          type: "customer",
+          fullName: customer.fullName,
+          phone: customer.phone,
+          centerName: customer.centerName,
+          villageName: customer.villageName,
+          marketName: customer.marketName,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
+const registerMerchant = async (req, res, next) => {
+  try {
+    const {
+      fullName,
+      storeName,
+      phone,
+      storeAddress,
+      commercialRegister,
+      governorate,
+      city,
+      village,
+      hasDelivery,
+      deliveryAreas,
+      password,
+      confirmPassword,
+      agreedToTerms,
+    } = req.body;
 
-exports.registerMerchant = async (req, res) => {
-  const {
-    fullName,
-    storeName,
-    phone,
-    storeAddress,
-    commercialRegister,
-    governorate,
-    city,
-    village,
-    hasDelivery,
-    deliveryAreas,
-    password,
-    confirmPassword,
-    agreedToTerms,
-  } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "كلمة المرور غير متطابقة",
+      });
+    }
 
-  if (password !== confirmPassword) {
-    return res.status(400).json({
-      success: false,
-      message: "كلمة المرور غير متطابقة",
+    const merchant = await Merchant.create({
+      fullName,
+      storeName,
+      phone,
+      storeAddress,
+      commercialRegister,
+      governorate,
+      city,
+      village,
+      hasDelivery,
+      deliveryAreas,
+      password,
+      agreedToTerms,
     });
-  }
 
-  const merchant = await Merchant.create({
-    fullName,
-    storeName,
-    phone,
-    storeAddress,
-    commercialRegister,
-    governorate,
-    city,
-    village,
-    hasDelivery,
-    deliveryAreas,
-    password,
-    agreedToTerms,
-  });
-
-  res.status(201).json({
-    success: true,
-    message: "تم إنشاء حساب التاجر بنجاح",
-    data: {
-      user: {
-        id: merchant._id,
-        type: "merchant",
-        fullName: merchant.fullName,
-        phone: merchant.phone,
-        storeName: merchant.storeName,
-        city: merchant.city,
-        village: merchant.village,
+    res.status(201).json({
+      success: true,
+      message: "تم إنشاء حساب التاجر بنجاح",
+      data: {
+        user: {
+          id: merchant._id,
+          type: "merchant",
+          fullName: merchant.fullName,
+          phone: merchant.phone,
+          storeName: merchant.storeName,
+          city: merchant.city,
+          village: merchant.village,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const login = async (req, res, next) => {
@@ -118,7 +125,7 @@ const login = async (req, res, next) => {
     if (error) {
       return res.status(400).json({
         success: false,
-        message: error.details[0].message
+        message: error.details[0].message,
       });
     }
 
@@ -127,7 +134,7 @@ const login = async (req, res, next) => {
     res.status(200).json({
       success: true,
       message: "تم تسجيل الدخول بنجاح",
-      data: result
+      data: result,
     });
   } catch (error) {
     next(error);
@@ -137,5 +144,5 @@ const login = async (req, res, next) => {
 module.exports = {
   registerCustomer,
   registerMerchant,
-  login
+  login,
 };
